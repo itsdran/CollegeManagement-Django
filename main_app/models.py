3ea420406d303dcfbb0c712173317ae4a43790b1
justdran <drflores2020@plm.edu.ph>
2022-12-26 22:04:49 +0800

    Updates

--- main_app/models.py
+++ main_app/models.py
@@ -93,8 +93,8 @@ class Staff(models.Model):
 
 class Subject(models.Model):
     name = models.CharField(max_length=120)
-    staff = models.ForeignKey(Staff,on_delete=models.CASCADE,)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff,on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, null=False, blank=False)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
@@ -113,8 +113,8 @@ class StudentSubjects (models.Model):
     staff = models.ForeignKey(Staff,on_delete=models.CASCADE)
     name = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
     first_name = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
-    #updated_at = models.DateTimeField(auto_now=True)
-    #created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name    
 
